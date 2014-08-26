51b100cfc805ba2e5b34023d70ef4a8cb1221cea
JaredCubilla <deraj.allibuc@gmail.com>
2014-08-26 16:00:44 -0400

    Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 				" *  <%= pkg.homepage %>\n" +
 				" *\n" +
 				" *  Made by <%= pkg.author.name %>\n" +
-				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" *  Licensed under <%= pkg.licenses[0].type %> \n" +
 				" */\n"
 		},
 
@@ -41,5 +41,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 
-	grunt.registerTask("default", ["jshint", "uglify"]);
+	grunt.registerTask("default", ["watch"]);
 };
